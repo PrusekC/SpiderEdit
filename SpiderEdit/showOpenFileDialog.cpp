ee9bbb4d604ea1490033a61f320f5df02fb64dc3
Patryk Cysarz <patrykcysarz@gmail.com>
2014-11-08 22:17:13 +0100

    Added dialog which asks user if he wants to save current file, before opening new

--- SpiderEdit/showOpenFileDialog.cpp
+++ SpiderEdit/showOpenFileDialog.cpp
@@ -2,6 +2,19 @@
 
 void showOpenFileDialog(GtkMenuItem *openFile, fileData *data)
 {
+    if(data->isSaved != true) // ask if user want to save current file
+    {
+        GtkWidget *saveQuestion;
+        saveQuestion = gtk_message_dialog_new(GTK_WINDOW(NULL), GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_QUESTION, GTK_BUTTONS_YES_NO, "Do you want to save your current file?");
+        gtk_window_set_title(GTK_WINDOW(saveQuestion), "Save?");
+
+        if(gtk_dialog_run(GTK_DIALOG(saveQuestion)) == -8) // user clicked "yes", show save file dialog
+        {
+            showSaveFileDialog(NULL, data);
+        }
+        gtk_widget_destroy(saveQuestion);
+    }
+
     GtkWidget *openFileDialog;
     openFileDialog = gtk_file_chooser_dialog_new("Open file", GTK_WINDOW(NULL), GTK_FILE_CHOOSER_ACTION_OPEN, GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL, GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT, NULL);
 
