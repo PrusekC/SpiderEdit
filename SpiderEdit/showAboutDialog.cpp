b2f3481607f1ab886362581ecc4f2f0eaa30647e
Patryk Cysarz <patrykcysarz@gmail.com>
2014-11-15 18:19:12 +0100

    Finished the about dialog // TODO: fix opening homepage

--- SpiderEdit/showAboutDialog.cpp
+++ SpiderEdit/showAboutDialog.cpp
@@ -2,5 +2,14 @@
 
 void showAboutDialog(GtkMenuItem *about)
 {
-    cout << "x";
+  GdkPixbuf *spiderEditIcon = gdk_pixbuf_new_from_file("icon.png", NULL);
+
+  GtkWidget *dialog = gtk_about_dialog_new();
+  gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(dialog), "(c) Patryk Cysarz");
+  gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(dialog), "SpiderEdit - Simple text editor");
+  gtk_about_dialog_set_website(GTK_ABOUT_DIALOG(dialog), "http://prusekc.github.io/SpiderEdit/");
+  gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(dialog), spiderEditIcon);
+  g_object_unref(spiderEditIcon), spiderEditIcon = NULL;
+  gtk_dialog_run(GTK_DIALOG(dialog));
+  gtk_widget_destroy(dialog);
 }
